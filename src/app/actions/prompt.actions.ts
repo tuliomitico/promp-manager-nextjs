d41170d51e79cce1d833c69519e61a8c90c73c9f
Túlio de Freitas Castro <tuliofc2008@hotmail.com>
2026-07-06 14:58:53 -0300

    feat: add create prompt action

--- src/app/actions/prompt.actions.ts
+++ src/app/actions/prompt.actions.ts
@@ -1,15 +1,56 @@
 'use server';
 
+import {
+  CreatePromptDTO,
+  createPromptSchema,
+} from '@/core/application/prompts/create-prompt.dto';
+import { CreatePromptUseCase } from '@/core/application/prompts/create-prompt.use-case';
 import { SearchPromptsUseCase } from '@/core/application/prompts/search-prompts.use-case';
 import { PromptSummary } from '@/core/domain/prompts/prompt.entity';
 import { PrismaPromptRepository } from '@/infra/repository/prisma-prompt.repository';
 import { prisma } from '@/lib/prisma';
+import z from 'zod';
 
 type SearchFormState = {
   success: boolean;
   prompts?: PromptSummary[];
   message?: string;
 };
+
+export async function createPromptAction(data: CreatePromptDTO) {
+  const validated = createPromptSchema.safeParse(data);
+
+  if (!validated.success) {
+    const { fieldErrors } = z.flattenError(validated.error);
+    return {
+      success: false,
+      message: 'Erro de validação',
+      errors: fieldErrors,
+    };
+  }
+
+  try {
+    const repository = new PrismaPromptRepository(prisma);
+    const useCase = new CreatePromptUseCase(repository);
+    await useCase.execute(validated.data);
+  } catch (error) {
+    const _error = error as Error;
+
+    if (_error.message === 'PROMPT_ALREADY_EXISTS') {
+      return {
+        success: false,
+        message: 'Este prompt já existe',
+      };
+    }
+
+    return { success: false, message: 'Falha ao criar prompt.' };
+  }
+
+  return {
+    success: true,
+    message: 'Prompt criado com sucesso!',
+  };
+}
 
 export async function searchPromptAction(
   _prev: SearchFormState,
