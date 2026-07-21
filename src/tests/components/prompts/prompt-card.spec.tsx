4e65a0362ae099746fcfab798f1c4c45f00d9c89
Túlio de Freitas Castro <tuliofc2008@hotmail.com>
2026-07-21 10:59:46 -0300

    test: ensure AlertDialog opens if clicked

--- src/tests/components/prompts/prompt-card.spec.tsx
+++ src/tests/components/prompts/prompt-card.spec.tsx
@@ -1,21 +1,33 @@
 import { PromptCard, type PromptCardProps } from '@/components/prompts';
 import { render, screen } from '@/lib/test-utils';
+import userEvent from '@testing-library/user-event';
 
 const makeSut = ({ prompt }: PromptCardProps) => {
   return render(<PromptCard prompt={prompt} />);
 };
 
 describe('PromptCard', () => {
+  const user = userEvent.setup();
+  const prompt: PromptCardProps['prompt'] = {
+    id: '1',
+    title: 'title 01',
+    content: 'content 01',
+  };
+
   it('deveria renderizar o link com href corretamente', () => {
-    const prompt: PromptCardProps['prompt'] = {
-      id: '1',
-      title: 'title 01',
-      content: 'content 01',
-    };
     makeSut({ prompt });
     const link = screen.getByRole('link');
 
     expect(link).toBeInTheDocument();
     expect(link).toHaveAttribute('href', `/${prompt.id}`);
+  });
+
+  it('deveria abrir o dialog de remoção de um prompt', async () => {
+    makeSut({ prompt });
+
+    const deleteButton = screen.getByRole('button', { name: 'Remover Prompt' });
+    await user.click(deleteButton);
+
+    expect(screen.getByText('Remover Prompt')).toBeInTheDocument();
   });
 });
